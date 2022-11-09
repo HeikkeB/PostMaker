77c59b9aff00ebd17477140ba31f34ecebebbae2
HeikkeB <vldbltn@gmail.com>
2022-11-09 20:19:17 +0300

    feat: make database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
 
-app.listen(5000, () => {
-  console.log('working')
-})
+//Constants 
+const PORT = process.env.PORT || 5001
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
+
+
+async function start() {
+  try {
+    await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.p8tssia.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`)
+
+    app.listen(PORT, ()=> console.log(`server started on port: ${PORT}`))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+start()
