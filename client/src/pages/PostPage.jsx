10ce8429b9ae54c42b617de40f55772e5b4b63e3
HeikkeB <vldbltn@gmail.com>
2022-11-27 14:30:55 +0300

    feat: make comments form

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -6,9 +6,11 @@ import {Link, useNavigate, useParams} from 'react-router-dom'
 import {useSelector, useDispatch} from 'react-redux'
 import {removePost} from '../redux/features/post/postSlice'
 import { toast } from 'react-toastify'
+import { createComment } from '../redux/features/comment/commentSlice'
 
 export default function PostPage() {
   const [post, setPost] = useState(null)
+  const [comment, setComment] = useState('')
   const {user} = useSelector((state) => state.auth)
   const params = useParams()
   const dispatch = useDispatch()
@@ -31,6 +33,16 @@ const handleRemovePost = () => {
 useEffect(() => {
   fetchPost()
 }, [fetchPost]);
+
+const handleSubmit = () => {
+  try {
+    const postId = params.id
+    dispatch(createComment({postId, comment}))
+    setComment('')
+  } catch (error) {
+    console.log(error)
+  }
+}
 
   if(!post) {
     return  <div className='text-xl text-center text-white py-10'>
@@ -82,7 +94,18 @@ useEffect(() => {
     </div>
       </div>
 
-      <div className='w-1/3'>COMMENTS</div>
+      <div className='w-1/3 p-8 bg-gray-700 flex flex-col gap-2 rounded-lg'>
+      <form className='flex gap-3' onSubmit={e => e.preventDefault()}>
+        <input 
+          type='text'
+          value={comment}
+          onChange={e => setComment(e.target.value)}
+          placeholder='Comment'
+          className='text-black w-full rounded-lg bg-gray-400 border p-2 text-xs outline-none placeholder:text-gray-700'
+        />
+        <button type='submit' onClick={handleSubmit} className='flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm py-2 px-1 w-[100px]'>SUBMIT</button>
+      </form>
+      </div>
     </div>
   </div>
 }
