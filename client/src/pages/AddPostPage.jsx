058d7cb10fe8eb21a9e9a639b607df3447093d97
HeikkeB <vldbltn@gmail.com>
2022-11-30 21:26:37 +0300

    feat: make add post page

--- client/src/pages/AddPostPage.jsx
+++ client/src/pages/AddPostPage.jsx
@@ -44,7 +44,7 @@ export default function AddPostPage() {
   return (
     <div>
       <form
-        className="w-1/3 mx-auto py-10"
+        className="w-1/3 mx-auto py-10 flex flex-col justify-center items-center"
         onSubmit={(e) => e.preventDefault()}
       >
         <label className="btnPageBig">
@@ -59,26 +59,24 @@ export default function AddPostPage() {
           {image && <img src={URL.createObjectURL(image)} alt={image.name} />}
         </div>
 
-        <label className="text-xs text-white opacity-70">
-          Title post:
+        <section className='w-full mb-6'>       
           <input
             type="text"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
             placeholder="Title post"
-            className="mt-1 mb-4 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700 placeholder:italic"
+            className="inputPageBig"
           />
-        </label>
-        <label className="text-xs text-white opacity-70">
-          Text post:
+        </section>
+        <section className='w-full mb-6'>        
           <textarea
             type="text"
             value={text}
             onChange={(e) => setText(e.target.value)}
             placeholder="Text post"
-            className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none resize-none h-44 placeholder:text-gray-700 placeholder:italic"
+            className="inputPageBig min-h-[90px]"
           />
-        </label>
+        </section>
 
         <div className="flex gap-10 items-center justify-center mt-4">
           <button
@@ -91,7 +89,7 @@ export default function AddPostPage() {
           <button
             type='button'
             onClick={clearFormHandler}
-            className="flex justify-center items-center bg-red-900 text-xs text-white rounded-sm py-2 px-1 w-[100px]"
+            className="btnPageRed"
           >
             CANCEL
           </button>
