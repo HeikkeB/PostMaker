3fd318e2482361237c047afa7dac57248a152ddd
HeikkeB <vldbltn@gmail.com>
2022-12-05 20:50:00 +0300

    feat: make heroku server

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  // baseURL: 'http://localhost:5000',
+  baseURL: 'https://postmaker.herokuapp.com',
 });
 
 instance.interceptors.request.use((config) => {
